33e4e5a7606746dfeafb22ad92b5a38546860247
Niclas Jareborg <niclas@jareborg.se>
2017-12-19 16:30:53 +0100

    First working version of time pruning script

--- prune_times.pl
+++ prune_times.pl
@@ -12,9 +12,12 @@ use JSON qw( from_json to_json );
 
 
 my $file = shift // die "Need input file\n";
+
 (my $outfile = $file) =~ s/(.+)\.(.+)/$1_pruned.$2/;
 print $file . "\n";
 print $outfile . "\n";
+
+open OF, ">$outfile" or die;
 
 my $json = from_json(slurp($file));
 
@@ -22,77 +25,58 @@ if ( ref($json) ne "ARRAY" ) {
     $json = [ $json ];
 }
 
-my $paskanakki = "Infrastructure:NBIS General:Admin::";
+my $paskanakki = "Infrastructure:NBIS General:Admin:"; # https://translate.google.se/?source=osdd#auto/en/paskanakki
 my $roundoff_tally = 0;
+my $rounded_sum = 0;
 
 for my $week (@$json) {
-    my $dt = $week->{'week'};
     while (my ($key, $entry) = each ($week->{'work'}) ) {
-      print "$key: $entry->{'duration'}\n";
       my $dur = $entry->{'duration'};
-      # my $dur1000 = 1000 * $dur;
-      # my $rem = $dur1000 % 500;
-      # print "$dur1000, rem: $rem\n";
-      # $dur =~  /\d+\.(\d*)/;
-      # my $tail = $1;
-      # print "$tail\n";
-      # $tail = "0.$tail";
-
       my $hour_int = int($dur);
       my $tail = $dur - $hour_int;
-      print "$tail\n";
 
-      # my $left = $tail - 0.5;
-      # print "$left\n";
       my $rounded;
-      if ($tail < 0.25) {
+      if ($tail < 0.33) {
         $rounded = floor($dur);
         $roundoff_tally += $tail;
       } elsif ($tail < 0.5) {
         $rounded = floor($dur) + 0.5;
         $roundoff_tally -= 0.5 - $tail;
-      } elsif ($tail < 0.75) {
+      } elsif ($tail < 0.83) {
         $rounded = floor($dur) + 0.5;
         $roundoff_tally += $tail - 0.5;
       } else {
         $rounded = ceil($dur);
         $roundoff_tally -= 1.0 - $tail;
       }
-      print "rounded: $rounded; tally: $roundoff_tally\n";
+
+      if($rounded == 0) {
+        delete $week->{'work'}->{$key};
+        print STDERR "Deleted $key: $dur\n";
+      }
+      $entry->{'duration'} = $rounded;
+      $rounded_sum += $rounded;
+      print STDERR "$key: $dur; rounded: $rounded; tally: $roundoff_tally\n";
     }
-    # for my $entry ( values %{ $week->{'work'} } ) {
-    #   print $entry;
-        # my $issue = get_issue_of( $entry );
-        # if ( ! $issue ) {
-        #     $error = 1;
-        #     next;
-        # }
-        #
-        # my $comment = $entry->{ $entry_mapping{comment} };
-        # if ($comment eq 'None') { $comment = ''; }
-        #
-        # my $activity_id = $activity_id_of{ $entry->{ $entry_mapping{activity} } };
-        #
-        # my $issue_title = issue_lookup($issue);
-        # if ( ! $issue_title ) {
-        #     say STDERR "WARNING: Can't find title of $issue";
-        #     $issue_title = "N/A ($issue)" . ' 'x30;
-        # }
-        #
-        # my %data = (
-        #     issue    => $issue,
-        #     activity => $activity_id,
-        #     activity_text => $activity_of_id{$activity_id},
-        #     dt       => $dt,
-        #     time     => $entry->{duration},
-        #     comment  => $comment,
-        #     entry    => $entry,
-        #     title    => $issue_title,
-        # );
-        #
-        # push @ok_entries, \%data;
-    # }
+    while (abs($roundoff_tally) > 0.5) {
+      if ($roundoff_tally >0) {
+        $week->{'work'}->{$paskanakki}->{'duration'} += 0.5;
+        $rounded_sum += 0.5;
+        $roundoff_tally -= 0.5;
+        print STDERR "Added 0.5 to $paskanakki\n";
+      } else {
+        $week->{'work'}->{$paskanakki}->{'duration'} -= 0.5;
+        $rounded_sum -= 0.5;
+        $roundoff_tally += 0.5;
+        print STDERR "Subtrackted 0.5 from $paskanakki\n";
+      }
+    }
+
+    $week->{'rounded_sum'} = $rounded_sum;
 }
+
+print OF to_json(@$json[0]) . "\n";
+close OF;
 
 sub slurp {
     my $file = shift;
